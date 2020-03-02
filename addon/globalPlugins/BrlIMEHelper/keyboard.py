c7e1d89108d5289b09a44669a02a2ede36184994
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2020-03-03 04:42:13 +0800

    Add KEYBOARD_MAPPING option
    This commit contains only the choice box and 4 out of 6 items.
    
    [!] Note that bopomofo_to_keys is currently equal to layout["STANDARD"].

--- addon/globalPlugins/BrlIMEHelper/keyboard.py
+++ addon/globalPlugins/BrlIMEHelper/keyboard.py
@@ -4,7 +4,29 @@
 # See the file LICENSE for more details.
 
 from __future__ import unicode_literals
+from collections import OrderedDict
 from winVersion import winVersion
+
+# Display names of keyboard mappings.
+# It must be ordered to keep the order of dialog options constant.
+mapping = OrderedDict()
+mapping["STANDARD"] = _("Standard")
+mapping["ET"] = _("E Tian")
+mapping["IBM"] = _("IBM")
+mapping["GIN_YIEH"] = _("Gin Yieh")
+# Hanyu Pinyin
+# Secondary Bopomofo Pinyin
+
+# Layouts of keyboard mappings.
+# Each layout is required to be a dict with all keys listed in layout_index.
+layout_index = "ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄧㄨㄩㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦ ˊˇˋ˙"
+layout = OrderedDict()
+layout["STANDARD"] = dict(zip(layout_index, "1qaz2wsxedcrfv5tgbyhnujm8ik,9ol.0p;/- 6347"))
+layout["ET"] = dict(zip(layout_index, "bpmfdtnlvkhg7c,./j;'sexuaorwiqzy890-= 2341"))
+layout["IBM"] = dict(zip(layout_index, "1234567890-qwertyuiopasdfghjkl;zxcvbn m,./"))
+layout["GIN_YIEH"] = dict(zip(layout_index, "2wsx3edcrfvtgb6yhnujm8ik,9ol.0p;/-['= qaz1"))
+
+assert(set(mapping.keys()) == set(layout.keys()))
 
 class _Symbol2KeyDict(dict):
     def __init__(self, *args, **kwargs):
@@ -21,10 +43,7 @@ class _Symbol2KeyDict(dict):
                 return "|".join("`u%04x" % (ord(index),))
             raise # Bopomofo IME on WinXP does not support characters outside the BMP.
 
-bopomofo_to_keys = dict(zip( # 標準注音鍵盤
-    "ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄧㄨㄩㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦ ˊˇˋ˙",
-    "1qaz2wsxedcrfv5tgbyhnujm8ik,9ol.0p;/- 6347"
-))
+bopomofo_to_keys = layout["STANDARD"]
 
 symb2gesture = _Symbol2KeyDict([
     (" ", " "),
