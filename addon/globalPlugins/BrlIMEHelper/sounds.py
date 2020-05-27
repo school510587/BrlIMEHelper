8c9e08063649953366f1d44b890ff3adea7568ce
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2020-05-28 03:07:54 +0800

    sounds.py: Add beep_enable and beep_disable
    Like beep_typo, they have corresponding wav files. On absence of the two
    files, DeviceConnect and DeviceDisconnect system sounds are played
    respectively.

--- addon/globalPlugins/BrlIMEHelper/sounds.py
+++ addon/globalPlugins/BrlIMEHelper/sounds.py
@@ -21,4 +21,6 @@ def _play_sound_hint(wav_path, replacement_winsound):
         log.error("Error playing sound file: {0}".format(wav_path), exc_info=True)
         PlaySound(replacement_winsound, SND_ALIAS|SND_ASYNC)
 
+beep_disable = lambda: _play_sound_hint("beep_disable.wav", "DeviceDisconnect")
+beep_enable = lambda: _play_sound_hint("beep_enable.wav", "DeviceConnect")
 beep_typo = lambda: _play_sound_hint("beep_typo.wav", "SystemExclamation")
