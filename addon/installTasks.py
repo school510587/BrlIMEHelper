283996a4cfacba0f05d81bffb95d4de652147e11
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2022-08-13 23:33:53 +0800

    installTasks.py: Remind the user to save configuration if necessary

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -9,11 +9,13 @@ from brailleTables import getTable as getBRLtable
 from logHandler import log
 import addonHandler
 import brailleInput
+import config
 import gui
 
 addonHandler.initTranslation()
 
 def onInstall():
+    dirty_conf = False
     try:
         us_comp8_table = getBRLtable("en-us-comp8-ext.utb")
     except:
@@ -27,4 +29,13 @@ def onInstall():
         wx.YES | wx.NO | wx.CENTER | wx.ICON_QUESTION
     ) == wx.YES:
         brailleInput.handler.table = us_comp8_table
+        dirty_conf = True
         log.info("The braille input translation table has been changed to en-us-comp8-ext.utb")
+    if dirty_conf and not config.conf["general"]["saveConfigurationOnExit"] and gui.messageBox(
+        # Translators: Dialog text shown to ask whether to save the user configuration.
+        _("The user configuration of NVDA has been changed. If it is not the first installation, then the reason would be the change of the option names/values of BrlIMEHelper. Would you like to save the change now?"),
+        # Translators: Title of the dialog shown to ask whether to save the user configuration.
+        _("Save the user configuration"), 
+        wx.YES | wx.NO | wx.CENTER | wx.ICON_QUESTION
+    ) == wx.YES:
+        config.conf.save()
