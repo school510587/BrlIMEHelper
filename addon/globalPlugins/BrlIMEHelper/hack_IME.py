b709efd6bb3c6ce8c6a730a9dd51660462c53569
Bo-Cheng Jhan <school510587@yahoo.com.tw>
2023-02-01 07:01:22 +0800

    Implement automatic IME search

--- addon/globalPlugins/BrlIMEHelper/hack_IME.py
+++ addon/globalPlugins/BrlIMEHelper/hack_IME.py
@@ -5,13 +5,17 @@
 
 from __future__ import unicode_literals
 from ctypes import *
+from time import time
 
+from keyboardHandler import KeyboardInputGesture
 from NVDAHelper import localLib
 from NVDAHelper import nvdaControllerInternal_inputConversionModeUpdate
 from NVDAHelper import nvdaControllerInternal_inputLangChangeNotify
 from NVDAHelper import _setDllFuncPointer
 from logHandler import log
+import queueHandler
 
+from . import configure
 from .runtime_state import thread_states
 
 # Note: Monkeying handleInputConversionModeUpdate does not work.
@@ -27,8 +31,13 @@ def hack_nvdaControllerInternal_inputConversionModeUpdate(oldFlags, newFlags, lc
         log.error("IME conversion mode update failure", exc_info=True)
     return nvdaControllerInternal_inputConversionModeUpdate(c_long(oldFlags), c_long(newFlags), c_ulong(lcid))
 
+_IME_search_start_time = None
+_target_IME_name = ""
+
 @WINFUNCTYPE(c_long, c_long, c_ulong, c_wchar_p)
 def hack_nvdaControllerInternal_inputLangChangeNotify(threadID, hkl, layoutString):
+    global _IME_search_start_time
+    global _target_IME_name
     global thread_states
     log.debug("IME language change: thread={0}, hkl={1}, layoutString={2}".format(threadID, hkl, layoutString))
     try:
@@ -36,11 +45,39 @@ def hack_nvdaControllerInternal_inputLangChangeNotify(threadID, hkl, layoutStrin
         log.debug("IME status: {0}".format(item))
     except:
         log.error("IME language change failure", exc_info=True)
+    silent = False
+    if _IME_search_start_time is not None:
+        if layoutString == _target_IME_name:
+            log.info("The IME is set to '{0}'".format(layoutString))
+            _IME_search_start_time = None
+            # configure.get("CBRLKB_MANUAL_TOGGLE_HINT") != "ui.message"?
+        elif time() - _IME_search_start_time > 0.5:
+            log.info("Failed IME search.")
+            _IME_search_start_time = None
+        else:
+            log.info("The current IME becomes '{0}'. The IME search continues.".format(layoutString))
+            queueHandler.queueFunction(queueHandler.eventQueue, KeyboardInputGesture.fromName("windows+space").send)
+            silent = True
+    if silent:
+        return 0
     return nvdaControllerInternal_inputLangChangeNotify(threadID, hkl, layoutString)
 
 def install():
     _setDllFuncPointer(localLib, "_nvdaControllerInternal_inputConversionModeUpdate", hack_nvdaControllerInternal_inputConversionModeUpdate)
     _setDllFuncPointer(localLib, "_nvdaControllerInternal_inputLangChangeNotify", hack_nvdaControllerInternal_inputLangChangeNotify)
+
+def start_IME_search(target_IME_name):
+    global _IME_search_start_time
+    global _target_IME_name
+    if _IME_search_start_time is not None:
+        if time() - _IME_search_start_time > 0.5:
+            _IME_search_start_time = None
+        else:
+            return False
+    _target_IME_name = target_IME_name
+    _IME_search_start_time = time()
+    KeyboardInputGesture.fromName("windows+shift+space").send()
+    return True
 
 def uninstall():
     _setDllFuncPointer(localLib, "_nvdaControllerInternal_inputConversionModeUpdate", nvdaControllerInternal_inputConversionModeUpdate)
